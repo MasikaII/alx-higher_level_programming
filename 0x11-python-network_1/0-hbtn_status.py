f65813e8f56e613538902caf02aef26bfb24c34a
Stephy <atienostephanie040@gmail.com>
2022-12-16 15:00:04 +0000

    request status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
-from urllib import request
+"""
+Module  for fetching the status of https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+
+def get_status():
+    """
+    Fetches the status of https://alx-intranet.hbtn.io/status
+    and returns the body of the response as a bytes object
+    """
+    with urllib.request.urlopen(
+            'https://alx-intranet.hbtn.io/status') as response:
+        return response.read()
+
 
 if __name__ == "__main__":
-    with request.urlopen("https://intranet.hbtn.io/status") as response:
-        response = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
+    # Get the status and print the body of the response
+    status = get_status()
+    print("Body response:")
+    print("\t- type:", type(status))
+    print("\t- content:", status)
+    print("\t- utf8 content:", status.decode('utf-8'))
