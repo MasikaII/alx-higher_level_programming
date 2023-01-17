418da55a66d8dacf2cafbad0d4d7215bbe32e3dd
Stephy <atienostephanie040@gmail.com>
2023-01-17 21:19:12 +0000

    request store

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// script gets the content of a webpage and
+// stores it in a file
+
+const fs = require('fs');
+const request = require('request');
+
+const url = process.argv[2];
+const filename = process.argv[3];
+
+request.get(url, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    fs.writeFile(filename, body, function (err) {
+      if (err) console.error(err);
+    });
+  } else {
+    console.error(error);
+  }
+});
