bb1731bea4fd07aad8eb0a7844e69d88aff45c14
Stephy <atienostephanie040@gmail.com>
2023-01-17 21:13:58 +0000

    starwars

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// script prints the title of a Star Wars movie
+
+const req = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+req.get(url, function (err, response, body) {
+  if (err) console.error(err); else {
+    const jsonbody = JSON.parse(body);
+    console.log(jsonbody.title);
+  }
+});
